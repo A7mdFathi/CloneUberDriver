fbbc0654a00b217462673615db6b34145fa06bc2
a7mefathi <a.fathi2112@gmail.com>
2019-02-09 23:07:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/androiduber/common/Common.java
@@ -0,0 +1,12 @@
+package com.example.android.androiduber.common;
+
+import com.example.android.androiduber.remote.IGoogleApi;
+import com.example.android.androiduber.remote.RetrofitClient;
+
+public class Common {
+    public static final String base_url="https://maps.googleapis.com";
+
+    public static IGoogleApi iGoogleApi(){
+        return RetrofitClient.getClient(base_url).create(IGoogleApi.class);
+    }
+}
---
+++ app/src/main/java/com/example/android/androiduber/remote/IGoogleApi.java
@@ -0,0 +1,10 @@
+package com.example.android.androiduber.remote;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface IGoogleApi {
+    @GET
+    Call<String> getPath(@Url String url);
+}
